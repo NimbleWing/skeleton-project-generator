fd48191da2544f2ce04a5a95af5717c879079865
305737824@qq.com <305737824@qq.com>
2021-08-09 15:35:57 +0800

    build(babel): 配置babel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Pure <305737824@qq.com>
+ * @CreateTime: 2021-08-09 14:56:04
+ * @LastEditors: Pure <305737824@qq.com>
+ * @LastEditTime: 2021-08-09 14:57:00
+ * @Description:
+ */
+import Os from 'os';
+console.log(Os.cpus());
