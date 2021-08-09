dd9a47b1210b0318c49f3ca4a0607b932356e589
305737824@qq.com <305737824@qq.com>
2021-08-09 17:41:42 +0800

    chore(eslint): 配置eslint

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
  * @Author: Pure <305737824@qq.com>
  * @CreateTime: 2021-08-09 14:56:04
  * @LastEditors: Pure <305737824@qq.com>
- * @LastEditTime: 2021-08-09 14:57:00
+ * @LastEditTime: 2021-08-09 17:14:32
  * @Description:
  */
 import Os from 'os';
+
 console.log(Os.cpus());
